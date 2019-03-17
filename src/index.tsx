0e586e23928babca125e979dfef46d0898a43ae0
kamienieckik <kamienieckik@gmail.com>
2019-03-17 23:22:16 +0100

    useReducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,31 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useReducer } from "react";
 import ReactDOM from "react-dom";
 
-const Store = React.createContext();
-
-export function Parent(props) {
-  const obj = { text: "random text" };
-  return <Store.Provider value={obj}>{props.children}</Store.Provider>;
-}
-
-export function Child() {
-  const hook = React.useContext(Store);
-  return <div>{hook.text}</div>;
+function App() {
+  const reducer = (state, action) => {
+    switch (action) {
+      case "ADD":
+        return state + 1;
+      case "SUB":
+        return state - 1;
+      case "RES":
+        return (state = 0);
+      default:
+        return state;
+    }
+    return state;
+  };
+  const [value, setValue] = useState("");
+  const [count, dispatch] = useReducer(reducer, 0);
+  return (
+    <Fragment>
+      <div>{count}</div>
+      <button onClick={() => dispatch("ADD")}>+</button>
+      <button onClick={() => dispatch("SUB")}>-</button>
+      <button onClick={() => dispatch("RES")}>reset</button>
+    </Fragment>
+  );
 }
 
 const root = document.getElementById("app-root");
-ReactDOM.render(
-  <Parent>
-    <Child />
-  </Parent>,
-  root
-);
+ReactDOM.render(<App />, root);
